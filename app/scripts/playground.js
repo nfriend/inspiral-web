34da3be037ff18171ef0793eda091c8647e926f9
Nathan Friend <contact@nathanfriend.com>
2014-07-16 20:55:55 -0500

    added favicon

--- app/scripts/playground.js
+++ app/scripts/playground.js
@@ -5,13 +5,13 @@ var toRadians = function (degrees) {
 var centerX = window.innerWidth / 2;
 var centerY = window.innerHeight / 2;
 var lineData = [];
+var lineData2 = [];
 
-var radius = 200;
-var largeRadius = 150;
-var smallAngle = 0;
+var radius = 110;
+var largeRadius = 100; var smallAngle = 0;
 var resolution = 10; // lower the better.
-var timeMultiplier = 19;
-var smallToLargeRatio = 1 / 19;
+var timeMultiplier = 2;
+var smallToLargeRatio = -1 / 2;
 
 for (var i = 0; i <= 360 * timeMultiplier; i = i + resolution) {
     var x = radius * Math.cos(toRadians(i)) + centerX;
@@ -20,6 +20,21 @@ for (var i = 0; i <= 360 * timeMultiplier; i = i + resolution) {
     var largeYAdjustment = largeRadius * Math.sin(toRadians(i * smallToLargeRatio));
 
     lineData.push({ 'x': x + largeXAdjustment, 'y': y + largeYAdjustment });
+}
+
+var radius = 100;
+var largeRadius = 50; var smallAngle = 0;
+var resolution = 10; // lower the better.
+var timeMultiplier = 2;
+var smallToLargeRatio = -1 / 2;
+
+for (var i = 0; i <= 360 * timeMultiplier; i = i + resolution) {
+    var x = radius * Math.cos(toRadians(i)) + centerX;
+    var y = radius * Math.sin(toRadians(i)) + centerY;
+    var largeXAdjustment = largeRadius * Math.cos(toRadians((i - 180) * smallToLargeRatio));
+    var largeYAdjustment = largeRadius * Math.sin(toRadians((i - 180) * smallToLargeRatio));
+
+    lineData2.push({ 'x': x + largeXAdjustment, 'y': y + largeYAdjustment });
 }
 
 //This is the accessor function we talked about above
@@ -37,18 +52,24 @@ var svgContainer = d3.select("body").append("svg")
 var path = svgContainer.append("path")
                             .attr("d", lineFunction(lineData))
                             .attr("stroke", "blue")
-                            .attr("stroke-width", 2)
+                            .attr("stroke-width", 18)
                             .attr("fill", "none");
 
-var totalLength = path.node().getTotalLength();
+var path = svgContainer.append("path")
+                            .attr("d", lineFunction(lineData2))
+                            .attr("stroke", "red")
+                            .attr("stroke-width", 18)
+                            .attr("fill", "none");
 
-var animationLength = 10000;
+//var totalLength = path.node().getTotalLength();
 
-path.attr("stroke-dasharray", totalLength + " " + totalLength)
-      .attr("stroke-dashoffset", totalLength)
-      .transition().duration(animationLength)
-        .ease("linear")
-        .attr("stroke-dashoffset", 0);
+//var animationLength = 3000;
+
+//path.attr("stroke-dasharray", totalLength + " " + totalLength)
+//      .attr("stroke-dashoffset", totalLength)
+//      .transition().duration(animationLength)
+//        .ease("linear")
+//        .attr("stroke-dashoffset", 0);
 
 svgContainer.on("click", function () {
     path.transition()
