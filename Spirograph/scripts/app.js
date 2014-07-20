66ac2864fd302987d30dcc24342a7e4789cff9c4
Nathan Friend <contact@nathanfriend.com>
2014-07-20 13:37:02 -0500

    initial canvas drawing working

--- Spirograph/scripts/app.js
+++ Spirograph/scripts/app.js
@@ -9,26 +9,62 @@ var Spirograph;
     ctx.lineWidth = 2;
     ctx.lineCap = 'round';
 
-    var previousCoords = null;
+    var svgContainer = d3.select("body").append("svg").attr("width", window.innerWidth).attr("height", window.innerHeight);
 
-    var canvasMouseMove = function (d, i) {
-        var mouseCoords = { x: d3.event.clientX, y: d3.event.clientY };
+    var gearOptions = (new Spirograph.Shapes.GearOptionsFactory()).Create(60);
+
+    var ringGearOptions = {
+        innerRadius: 192,
+        innerToothCount: 96,
+        innerToothHeight: 10,
+        outerRadius: 288,
+        outerToothCount: 144,
+        outerToothHeight: 10
+    };
+
+    var ringGear = svgContainer.append("g").attr("class", "gear ring-gear").attr("transform", "translate(" + Spirograph.Utility.getCenterX() + "," + Spirograph.Utility.getCenterY() + ")").datum(ringGearOptions).append("path").attr("d", Spirograph.Shapes.RingGear);
+
+    var gear = svgContainer.append("g").attr("class", "gear").datum(gearOptions).append("path").attr("d", Spirograph.Shapes.Gear);
+
+    var previousCoords = {
+        x: (ringGearOptions.innerRadius - gearOptions.radius - 2 + Spirograph.Utility.getCenterX()),
+        y: Spirograph.Utility.getCenterY()
+    };
+
+    var svgContainerMouseMove = function (d, i) {
+        var mouseCoords = Spirograph.Utility.toStandardCoords({ x: d3.event.clientX, y: d3.event.clientY }, { x: window.innerWidth, y: window.innerHeight });
+        var radius = ringGearOptions.innerRadius - gearOptions.radius - 2;
+        var mouseAngle = Math.atan2(mouseCoords.y, mouseCoords.x);
+
+        var newX = radius * Math.cos(mouseAngle) + Spirograph.Utility.getCenterX();
+        var newY = -1 * radius * Math.sin(mouseAngle) + Spirograph.Utility.getCenterY();
+
+        var gearRotation = 360 * (((Spirograph.Utility.toDegrees(mouseAngle) / 360) * 2 * Math.PI * ringGearOptions.innerRadius) / (2 * Math.PI * gearOptions.radius));
+        gearRotation -= Spirograph.Utility.toDegrees(mouseAngle);
+
+        $('#output').html('<p>Mouse angle: ' + Spirograph.Utility.toDegrees(mouseAngle) + '</p><p>Gear angle: ' + gearRotation + '</p>');
+
+        gear.attr("transform", "translate(" + newX + "," + newY + ") rotate(" + gearRotation + ")");
+
         ctx.beginPath();
         ctx.moveTo(previousCoords.x, previousCoords.y);
-        ctx.lineTo(mouseCoords.x, mouseCoords.y);
+        ctx.lineTo(newX, newY);
         ctx.stroke();
         ctx.closePath();
 
-        previousCoords = mouseCoords;
+        previousCoords = {
+            x: newX,
+            y: newY
+        };
     };
 
-    canvas.on('mousedown', function (d, i) {
-        previousCoords = { x: d3.event.clientX, y: d3.event.clientY };
-        canvas.on('mousemove', canvasMouseMove);
+    gear.on("mousedown", function (d, i) {
+        svgContainer.on("mousemove", svgContainerMouseMove);
+        svgContainer.on("mouseup", function () {
+            svgContainer.on("mousemove", null);
+        });
     });
 
-    canvas.on('mouseup', function () {
-        canvas.on('mousemove', null);
-    });
+    gear.attr("transform", "translate(" + (ringGearOptions.innerRadius - gearOptions.radius - 2 + Spirograph.Utility.getCenterX()) + "," + Spirograph.Utility.getCenterY() + ") rotate(" + 0 + ")");
 })(Spirograph || (Spirograph = {}));
 //# sourceMappingURL=app.js.map
--- Spirograph/scripts/app.ts
+++ Spirograph/scripts/app.ts
@@ -12,74 +12,69 @@ module Spirograph {
     ctx.lineWidth = 2;
     ctx.lineCap = 'round';
 
-    var previousCoords = null;
+    var svgContainer = d3.select("body").append("svg").attr("width", window.innerWidth).attr("height", window.innerHeight);
 
-    var canvasMouseMove = (d, i) => {
-        var mouseCoords = { x: <number> d3.event.clientX, y: <number> d3.event.clientY };
+    var gearOptions = (new Shapes.GearOptionsFactory()).Create(60);
+
+    var ringGearOptions: Shapes.RingGearOptions = {
+        innerRadius: 192,
+        innerToothCount: 96,
+        innerToothHeight: 10,
+        outerRadius: 288,
+        outerToothCount: 144,
+        outerToothHeight: 10
+    };
+
+    var ringGear = svgContainer.append("g")
+        .attr("class", "gear ring-gear")
+        .attr("transform", "translate(" + Utility.getCenterX() + "," + Utility.getCenterY() + ")")
+        .datum(ringGearOptions)
+        .append("path")
+        .attr("d", Shapes.RingGear);
+
+
+    var gear = svgContainer.append("g")
+        .attr("class", "gear")
+        .datum(gearOptions)
+        .append("path")
+        .attr("d", Shapes.Gear);
+
+    var previousCoords = {
+        x: (ringGearOptions.innerRadius - gearOptions.radius - 2 + Utility.getCenterX()),
+        y: Utility.getCenterY()
+    }
+
+    var svgContainerMouseMove = function (d, i) {
+        var mouseCoords = Utility.toStandardCoords({ x: d3.event.clientX, y: d3.event.clientY }, { x: window.innerWidth, y: window.innerHeight });
+        var radius = ringGearOptions.innerRadius - gearOptions.radius - 2;
+        var mouseAngle = Math.atan2(mouseCoords.y, mouseCoords.x);
+
+        var newX = radius * Math.cos(mouseAngle) + Utility.getCenterX();
+        var newY = -1 * radius * Math.sin(mouseAngle) + Utility.getCenterY();
+
+        var gearRotation = 360 * (((Utility.toDegrees(mouseAngle) / 360) * 2 * Math.PI * ringGearOptions.innerRadius) / (2 * Math.PI * gearOptions.radius));
+        gearRotation -= Utility.toDegrees(mouseAngle);
+
+        $('#output').html('<p>Mouse angle: ' + Utility.toDegrees(mouseAngle) + '</p><p>Gear angle: ' + gearRotation + '</p>');
+
+        gear.attr("transform", "translate(" + newX + "," + newY + ") rotate(" + gearRotation + ")");
+
         ctx.beginPath();
         ctx.moveTo(previousCoords.x, previousCoords.y);
-        ctx.lineTo(mouseCoords.x, mouseCoords.y);
+        ctx.lineTo(newX, newY);
         ctx.stroke();
         ctx.closePath();
 
-        previousCoords = mouseCoords;
-    };
+        previousCoords = {
+            x: newX,
+            y: newY
+        };
+    }
 
-    canvas.on('mousedown', (d, i) => {
-        previousCoords = { x: <number> d3.event.clientX, y: <number> d3.event.clientY };
-        canvas.on('mousemove', canvasMouseMove);
+    gear.on("mousedown", function (d, i) {
+        svgContainer.on("mousemove", svgContainerMouseMove);
+        svgContainer.on("mouseup", function () { svgContainer.on("mousemove", null); });
     });
 
-    canvas.on('mouseup', () => { canvas.on('mousemove', null); });
-
-    //var svgContainer = d3.select("body").append("svg").attr("width", window.innerWidth).attr("height", window.innerHeight);
-
-    //var gearOptions = (new Shapes.GearOptionsFactory()).Create(60);
-
-    //var ringGearOptions: Shapes.RingGearOptions = {
-    //    innerRadius: 192,
-    //    innerToothCount: 96,
-    //    innerToothHeight: 10,
-    //    outerRadius: 288,
-    //    outerToothCount: 144,
-    //    outerToothHeight: 10
-    //};
-
-    //var ringGear = svgContainer.append("g")
-    //    .attr("class", "gear ring-gear")
-    //    .attr("transform", "translate(" + Utility.getCenterX() + "," + Utility.getCenterY() + ")")
-    //    .datum(ringGearOptions)
-    //    .append("path")
-    //    .attr("d", Shapes.RingGear);
-
-
-    //var gear = svgContainer.append("g")
-    //    .attr("class", "gear")
-    //    .datum(gearOptions)
-    //    .append("path")
-    //    .attr("d", Shapes.Gear);
-
-    //var svgContainerMouseMove = function (d, i) {
-    //    var mouseCoords = Utility.toStandardCoords({ x: d3.event.clientX, y: d3.event.clientY }, { x: window.innerWidth, y: window.innerHeight });
-    //    var radius = ringGearOptions.innerRadius - gearOptions.radius - 2;
-    //    var mouseAngle = Math.atan2(mouseCoords.y, mouseCoords.x);
-
-    //    var newX = radius * Math.cos(mouseAngle) + Utility.getCenterX();
-    //    var newY = -1 * radius * Math.sin(mouseAngle) + Utility.getCenterY();
-
-    //    var gearRotation = 360 * (((Utility.toDegrees(mouseAngle) / 360) * 2 * Math.PI * ringGearOptions.innerRadius) / (2 * Math.PI * gearOptions.radius));
-    //    gearRotation -= Utility.toDegrees(mouseAngle);
-
-    //    $('#output').html('<p>Mouse angle: ' + Utility.toDegrees(mouseAngle) + '</p><p>Gear angle: ' + gearRotation + '</p>');
-
-    //    gear.attr("transform", "translate(" + newX + "," + newY + ") rotate(" + gearRotation + ")");
-    //    //gear.attr("transform", "rotate(" + Utility.toDegrees(-1 * mouseAngle) + ")");
-    //}
-
-    //gear.on("mousedown", function (d, i) {
-    //    svgContainer.on("mousemove", svgContainerMouseMove);
-    //    svgContainer.on("mouseup", function () { svgContainer.on("mousemove", null); });
-    //});
-
-    //gear.attr("transform", "translate(" + (ringGearOptions.innerRadius - gearOptions.radius - 2 + Utility.getCenterX()) + "," + Utility.getCenterY() + ") rotate(" + 0 + ")");
+    gear.attr("transform", "translate(" + (ringGearOptions.innerRadius - gearOptions.radius - 2 + Utility.getCenterX()) + "," + Utility.getCenterY() + ") rotate(" + 0 + ")");
 }
