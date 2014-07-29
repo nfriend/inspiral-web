0c3bceba536d531d4989e81347993f8db917bae1
Nathan Friend <contact@nathanfriend.com>
2014-07-28 22:44:52 -0500

    fixed RingGear teeth alignment

--- Spirograph/scripts/Shapes/RingGear.js
+++ Spirograph/scripts/Shapes/RingGear.js
@@ -17,10 +17,11 @@ var Spirograph;
             // draw the circle and outer teeth
             pathBuilder.addCommand(new Spirograph.SVG.MCommand(options.outerRadius - options.outerToothHeight, 0));
             for (var i = 0; i < options.outerToothCount; i++) {
-                pathBuilder.addCommand(new Spirograph.SVG.ACommand(options.outerRadius - options.outerToothHeight, options.outerRadius - options.outerToothHeight, 0, false, true, (options.outerRadius - options.outerToothHeight) * Math.cos(Spirograph.Utility.toRadians(angle + delta * 1 / 6)), (options.outerRadius - options.outerToothHeight) * Math.sin(Spirograph.Utility.toRadians(angle + delta * 1 / 6))));
-                pathBuilder.addCommand(new Spirograph.SVG.LCommand(options.outerRadius * Math.cos(Spirograph.Utility.toRadians(angle + delta * 1 / 2)), options.outerRadius * Math.sin(Spirograph.Utility.toRadians(angle + delta * 1 / 2))));
-                pathBuilder.addCommand(new Spirograph.SVG.ACommand(options.outerRadius, options.outerRadius, 0, false, true, options.outerRadius * Math.cos(Spirograph.Utility.toRadians(angle + delta * 4 / 6)), options.outerRadius * Math.sin(Spirograph.Utility.toRadians(angle + delta * 4 / 6))));
-                pathBuilder.addCommand(new Spirograph.SVG.LCommand((options.outerRadius - options.outerToothHeight) * Math.cos(Spirograph.Utility.toRadians(angle + delta)), (options.outerRadius - options.outerToothHeight) * Math.sin(Spirograph.Utility.toRadians(angle + delta))));
+                pathBuilder.addCommand(new Spirograph.SVG.ACommand(options.outerRadius - options.outerToothHeight, options.outerRadius - options.outerToothHeight, 0, false, true, (options.outerRadius - options.outerToothHeight) * Math.cos(Spirograph.Utility.toRadians(angle + delta * 1 / 12)), (options.outerRadius - options.outerToothHeight) * Math.sin(Spirograph.Utility.toRadians(angle + delta * 1 / 12))));
+                pathBuilder.addCommand(new Spirograph.SVG.LCommand(options.outerRadius * Math.cos(Spirograph.Utility.toRadians(angle + delta * 5 / 12)), options.outerRadius * Math.sin(Spirograph.Utility.toRadians(angle + delta * 5 / 12))));
+                pathBuilder.addCommand(new Spirograph.SVG.ACommand(options.outerRadius, options.outerRadius, 0, false, true, options.outerRadius * Math.cos(Spirograph.Utility.toRadians(angle + delta * 7 / 12)), options.outerRadius * Math.sin(Spirograph.Utility.toRadians(angle + delta * 7 / 12))));
+                pathBuilder.addCommand(new Spirograph.SVG.LCommand((options.outerRadius - options.outerToothHeight) * Math.cos(Spirograph.Utility.toRadians(angle + delta * 11 / 12)), (options.outerRadius - options.outerToothHeight) * Math.sin(Spirograph.Utility.toRadians(angle + delta * 11 / 12))));
+                pathBuilder.addCommand(new Spirograph.SVG.ACommand(options.outerRadius - options.outerToothHeight, options.outerRadius - options.outerToothHeight, 0, false, true, (options.outerRadius - options.outerToothHeight) * Math.cos(Spirograph.Utility.toRadians(angle + delta * 12 / 12)), (options.outerRadius - options.outerToothHeight) * Math.sin(Spirograph.Utility.toRadians(angle + delta * 12 / 12))));
                 angle += delta;
             }
 
@@ -32,10 +33,11 @@ var Spirograph;
             // remove the center and draw the inner teeth
             pathBuilder.addCommand(new Spirograph.SVG.MCommand(options.innerRadius, 0));
             for (var i = 0; i < options.innerToothCount; i++) {
-                pathBuilder.addCommand(new Spirograph.SVG.ACommand(options.innerRadius, options.innerRadius, 0, false, true, options.innerRadius * Math.cos(Spirograph.Utility.toRadians(angle + delta * 1 / 6)), options.innerRadius * Math.sin(Spirograph.Utility.toRadians(angle + delta * 1 / 6))));
-                pathBuilder.addCommand(new Spirograph.SVG.LCommand((options.innerRadius + options.innerToothHeight) * Math.cos(Spirograph.Utility.toRadians(angle + delta * 1 / 2)), (options.innerRadius + options.innerToothHeight) * Math.sin(Spirograph.Utility.toRadians(angle + delta * 1 / 2))));
-                pathBuilder.addCommand(new Spirograph.SVG.ACommand(options.innerRadius + options.innerToothHeight, options.innerRadius + options.innerToothHeight, 0, false, true, (options.innerRadius + options.innerToothHeight) * Math.cos(Spirograph.Utility.toRadians(angle + delta * 4 / 6)), (options.innerRadius + options.innerToothHeight) * Math.sin(Spirograph.Utility.toRadians(angle + delta * 4 / 6))));
-                pathBuilder.addCommand(new Spirograph.SVG.LCommand(options.innerRadius * Math.cos(Spirograph.Utility.toRadians(angle + delta)), options.innerRadius * Math.sin(Spirograph.Utility.toRadians(angle + delta))));
+                pathBuilder.addCommand(new Spirograph.SVG.ACommand(options.innerRadius, options.innerRadius, 0, false, true, options.innerRadius * Math.cos(Spirograph.Utility.toRadians(angle + delta * 1 / 12)), options.innerRadius * Math.sin(Spirograph.Utility.toRadians(angle + delta * 1 / 12))));
+                pathBuilder.addCommand(new Spirograph.SVG.LCommand((options.innerRadius + options.innerToothHeight) * Math.cos(Spirograph.Utility.toRadians(angle + delta * 5 / 12)), (options.innerRadius + options.innerToothHeight) * Math.sin(Spirograph.Utility.toRadians(angle + delta * 5 / 12))));
+                pathBuilder.addCommand(new Spirograph.SVG.ACommand(options.innerRadius + options.innerToothHeight, options.innerRadius + options.innerToothHeight, 0, false, true, (options.innerRadius + options.innerToothHeight) * Math.cos(Spirograph.Utility.toRadians(angle + delta * 7 / 12)), (options.innerRadius + options.innerToothHeight) * Math.sin(Spirograph.Utility.toRadians(angle + delta * 7 / 12))));
+                pathBuilder.addCommand(new Spirograph.SVG.LCommand(options.innerRadius * Math.cos(Spirograph.Utility.toRadians(angle + delta * 11 / 12)), options.innerRadius * Math.sin(Spirograph.Utility.toRadians(angle + delta * 11 / 12))));
+                pathBuilder.addCommand(new Spirograph.SVG.ACommand(options.innerRadius, options.innerRadius, 0, false, true, options.innerRadius * Math.cos(Spirograph.Utility.toRadians(angle + delta * 12 / 12)), options.innerRadius * Math.sin(Spirograph.Utility.toRadians(angle + delta * 12 / 12))));
                 angle += delta;
             }
 
--- Spirograph/scripts/Shapes/RingGear.ts
+++ Spirograph/scripts/Shapes/RingGear.ts
@@ -18,10 +18,11 @@ module Spirograph.Shapes {
         // draw the circle and outer teeth
         pathBuilder.addCommand(new SVG.MCommand(options.outerRadius - options.outerToothHeight, 0));
         for (var i = 0; i < options.outerToothCount; i++) {
-            pathBuilder.addCommand(new SVG.ACommand(options.outerRadius - options.outerToothHeight, options.outerRadius - options.outerToothHeight, 0, false, true, (options.outerRadius - options.outerToothHeight) * Math.cos(Utility.toRadians(angle + delta * 1 / 6)), (options.outerRadius - options.outerToothHeight) * Math.sin(Utility.toRadians(angle + delta * 1 / 6))));
-            pathBuilder.addCommand(new SVG.LCommand(options.outerRadius * Math.cos(Utility.toRadians(angle + delta * 1 / 2)), options.outerRadius * Math.sin(Utility.toRadians(angle + delta * 1 / 2))));
-            pathBuilder.addCommand(new SVG.ACommand(options.outerRadius, options.outerRadius, 0, false, true, options.outerRadius * Math.cos(Utility.toRadians(angle + delta * 4 / 6)), options.outerRadius * Math.sin(Utility.toRadians(angle + delta * 4 / 6))));
-            pathBuilder.addCommand(new SVG.LCommand((options.outerRadius - options.outerToothHeight) * Math.cos(Utility.toRadians(angle + delta)), (options.outerRadius - options.outerToothHeight) * Math.sin(Utility.toRadians(angle + delta))));
+            pathBuilder.addCommand(new SVG.ACommand(options.outerRadius - options.outerToothHeight, options.outerRadius - options.outerToothHeight, 0, false, true, (options.outerRadius - options.outerToothHeight) * Math.cos(Utility.toRadians(angle + delta * 1 / 12)), (options.outerRadius - options.outerToothHeight) * Math.sin(Utility.toRadians(angle + delta * 1 / 12))));
+            pathBuilder.addCommand(new SVG.LCommand(options.outerRadius * Math.cos(Utility.toRadians(angle + delta * 5 / 12)), options.outerRadius * Math.sin(Utility.toRadians(angle + delta * 5 / 12))));
+            pathBuilder.addCommand(new SVG.ACommand(options.outerRadius, options.outerRadius, 0, false, true, options.outerRadius * Math.cos(Utility.toRadians(angle + delta * 7 / 12)), options.outerRadius * Math.sin(Utility.toRadians(angle + delta * 7 / 12))));
+            pathBuilder.addCommand(new SVG.LCommand((options.outerRadius - options.outerToothHeight) * Math.cos(Utility.toRadians(angle + delta * 11 / 12)), (options.outerRadius - options.outerToothHeight) * Math.sin(Utility.toRadians(angle + delta * 11 / 12))));
+            pathBuilder.addCommand(new SVG.ACommand(options.outerRadius - options.outerToothHeight, options.outerRadius - options.outerToothHeight, 0, false, true, (options.outerRadius - options.outerToothHeight) * Math.cos(Utility.toRadians(angle + delta * 12 / 12)), (options.outerRadius - options.outerToothHeight) * Math.sin(Utility.toRadians(angle + delta * 12 / 12))));
             angle += delta;
         }
 
@@ -33,10 +34,11 @@ module Spirograph.Shapes {
         // remove the center and draw the inner teeth
         pathBuilder.addCommand(new SVG.MCommand(options.innerRadius, 0));
         for (var i = 0; i < options.innerToothCount; i++) {
-            pathBuilder.addCommand(new SVG.ACommand(options.innerRadius, options.innerRadius, 0, false, true, options.innerRadius * Math.cos(Utility.toRadians(angle + delta * 1 / 6)), options.innerRadius * Math.sin(Utility.toRadians(angle + delta * 1 / 6))));
-            pathBuilder.addCommand(new SVG.LCommand((options.innerRadius + options.innerToothHeight) * Math.cos(Utility.toRadians(angle + delta * 1 / 2)), (options.innerRadius + options.innerToothHeight) * Math.sin(Utility.toRadians(angle + delta * 1 / 2))));
-            pathBuilder.addCommand(new SVG.ACommand(options.innerRadius + options.innerToothHeight, options.innerRadius + options.innerToothHeight, 0, false, true, (options.innerRadius + options.innerToothHeight) * Math.cos(Utility.toRadians(angle + delta * 4 / 6)), (options.innerRadius + options.innerToothHeight) * Math.sin(Utility.toRadians(angle + delta * 4 / 6))));
-            pathBuilder.addCommand(new SVG.LCommand(options.innerRadius * Math.cos(Utility.toRadians(angle + delta)), options.innerRadius * Math.sin(Utility.toRadians(angle + delta))));
+            pathBuilder.addCommand(new SVG.ACommand(options.innerRadius, options.innerRadius, 0, false, true, options.innerRadius * Math.cos(Utility.toRadians(angle + delta * 1 / 12)), options.innerRadius * Math.sin(Utility.toRadians(angle + delta * 1 / 12))));
+            pathBuilder.addCommand(new SVG.LCommand((options.innerRadius + options.innerToothHeight) * Math.cos(Utility.toRadians(angle + delta * 5 / 12)), (options.innerRadius + options.innerToothHeight) * Math.sin(Utility.toRadians(angle + delta * 5 / 12))));
+            pathBuilder.addCommand(new SVG.ACommand(options.innerRadius + options.innerToothHeight, options.innerRadius + options.innerToothHeight, 0, false, true, (options.innerRadius + options.innerToothHeight) * Math.cos(Utility.toRadians(angle + delta * 7 / 12)), (options.innerRadius + options.innerToothHeight) * Math.sin(Utility.toRadians(angle + delta * 7 / 12))));
+            pathBuilder.addCommand(new SVG.LCommand(options.innerRadius * Math.cos(Utility.toRadians(angle + delta * 11 / 12)), options.innerRadius * Math.sin(Utility.toRadians(angle + delta * 11 / 12))));
+            pathBuilder.addCommand(new SVG.ACommand(options.innerRadius, options.innerRadius, 0, false, true, options.innerRadius * Math.cos(Utility.toRadians(angle + delta * 12 / 12)), options.innerRadius * Math.sin(Utility.toRadians(angle + delta * 12 / 12))));
             angle += delta;
         }
 
